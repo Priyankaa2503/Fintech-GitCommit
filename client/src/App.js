8cfbb3b8ea75e59ccca11097243e5311a2125ae3
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-03-18 15:32:55 +0530

    added client

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useEffect,useState } from "react";
+import './App.css';
+
+function App() {
+  const [data,setData] = useState([{}]);
+  useEffect(() => {
+    fetch("/members").then(
+      res=>res.json()
+
+    ).then(
+      data=>{
+        setData(data)
+        console.log(data)
+      }
+    )
+
+  }, [])
+  
+
+
+  return (
+    <div className="App">
+
+      {(typeof data.members==='undefined')?(
+        <p>Loading...</p>
+      ):(
+        data.members.map((member,i)=>(
+          < p key={i}>{member}</p>
+        ))
+      )}
+      
+    </div>
+  );
+}
+
+export default App;
