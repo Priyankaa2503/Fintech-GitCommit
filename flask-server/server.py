97f7ce4facd940c2f04685008eb52afcd3994365
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-03-18 15:15:22 +0530

    first commit

---
+++ flask-server/server.py
@@ -0,0 +1,16 @@
+# from flask import Flask
+# app = Flask (__name__)
+# @app.route("/members")
+# def members():
+#         return {"members":["mem1","mem2","mem3"]}
+# if __name__=="__main__":
+#         app.run(debug=True)
+from PIL import Image
+import pytesseract
+
+image_1='image1.png'
+
+
+img_obj_1=Image.open(image_1)
+text_1 = pytesseract.image_to_string(img_obj_1)
+print(text_1)
